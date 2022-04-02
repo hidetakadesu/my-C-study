4b61ff75f479baa803fcb1c65830180163bf700c
Kaylor Lira | H I D E T E K <75343608+kaylorlira@users.noreply.github.com>
2022-04-01 22:01:04 -0300

    Update Ponto1_Ponto2.cpp

--- Ponto1_Ponto2.cpp
+++ Ponto1_Ponto2.cpp
@@ -9,5 +9,5 @@ int main(){
 	printf("Onde se encontra o ponto 2? Digite no formato x,y\n");
 	scanf("%f,%f",&vx2,&vy2);
 res=sqrt(pow((vx2-vx1),2)+pow((vy2-vy1),2));
-	printf("\nA distancia entre os dois pontos sera de %.4f.",res);
+	printf("\nA distancia entre os dois pontos (ponto um esta em %.4f,%.4f e o ponto dois esta em %.4f,%.4f)sera de %.4f.",vx1,vy1,vx2,vy2,res);
 }

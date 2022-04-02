43d99f59736fd14414abf9055abd51f463e8864d
Kaylor Lira | H I D E T E K <75343608+kaylorlira@users.noreply.github.com>
2022-04-01 22:56:09 -0300

    Calculate if a number is even or odd

--- even_odd.cpp
+++ even_odd.cpp
@@ -6,10 +6,10 @@ int main(){
 	printf("What is the number?");
 	scanf("%d",&req);
 	if(0==req%2){
-		printf("This number is even");
+		printf("This number is even.");
 	}
 	else{
-		printf("This number is odd");
+		printf("This number is odd.");
 	}
 	return 0;
 }
